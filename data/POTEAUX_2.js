5fc2e28c816241076d25b3ea953551629f87513a
AlbertoAlbert <76694036+AlbertoAlbert@users.noreply.github.com>
2020-12-31 12:18:54 +0200

    Add files via upload

--- data/POTEAUX_2.js
+++ data/POTEAUX_2.js
@@ -2,9 +2,9 @@ var json_POTEAUX_2 = {
 "type": "FeatureCollection",
 "name": "POTEAUX_2",
 "features": [
-{ "type": "Feature", "properties": { "ID": 1.0, "STATUS": "Up", "Stat_Numb": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -0.725861760515006, -10.386176422375001 ] } },
-{ "type": "Feature", "properties": { "ID": 2.0, "STATUS": "Down", "Stat_Numb": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -4.73349534830334, -9.464420697183678 ] } },
-{ "type": "Feature", "properties": { "ID": 3.0, "STATUS": "Down", "Stat_Numb": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 0.276046636432078, -4.33464970481461 ] } },
-{ "type": "Feature", "properties": { "ID": 4.0, "STATUS": "Up", "Stat_Numb": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -2.28883885975245, -7.18006955214433 ] } }
+{ "type": "Feature", "properties": { "ID": 1.0, "STATUS": "Up", "Stat_Numb": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.604315138955101, 33.969286736641195 ] } },
+{ "type": "Feature", "properties": { "ID": 2.0, "STATUS": "Down", "Stat_Numb": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 35.542791701455101, 33.871835490219397 ] } },
+{ "type": "Feature", "properties": { "ID": 3.0, "STATUS": "Down", "Stat_Numb": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 35.5891299648139, 33.941029788883597 ] } },
+{ "type": "Feature", "properties": { "ID": 4.0, "STATUS": "Up", "Stat_Numb": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.579110880844397, 33.909093958730899 ] } }
 ]
 }
