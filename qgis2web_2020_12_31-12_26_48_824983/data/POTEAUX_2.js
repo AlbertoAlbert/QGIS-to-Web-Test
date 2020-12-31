a3d9ea23e2b0c04808b2c561ab894d7990e9faf3
AlbertoAlbert <76694036+AlbertoAlbert@users.noreply.github.com>
2020-12-31 12:29:33 +0200

    Add files via upload

---
+++ qgis2web_2020_12_31-12_26_48_824983/data/POTEAUX_2.js
@@ -0,0 +1,10 @@
+var json_POTEAUX_2 = {
+"type": "FeatureCollection",
+"name": "POTEAUX_2",
+"features": [
+{ "type": "Feature", "properties": { "ID": 1.0, "STATUS": "Up", "Stat_Numb": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.604315138955101, 33.969286736641195 ] } },
+{ "type": "Feature", "properties": { "ID": 2.0, "STATUS": "Down", "Stat_Numb": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 35.542791701455101, 33.871835490219397 ] } },
+{ "type": "Feature", "properties": { "ID": 3.0, "STATUS": "Down", "Stat_Numb": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 35.5891299648139, 33.941029788883597 ] } },
+{ "type": "Feature", "properties": { "ID": 4.0, "STATUS": "Up", "Stat_Numb": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 35.579110880844397, 33.909093958730899 ] } }
+]
+}
