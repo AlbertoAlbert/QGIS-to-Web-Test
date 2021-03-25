f31b353f8c1d9994e685bb277027e2788edd9fc8
AlbertoAlbert <76694036+AlbertoAlbert@users.noreply.github.com>
2021-03-25 15:02:08 +0200

    Add files via upload

---
+++ data/Lines_2.js
@@ -0,0 +1,10 @@
+var json_Lines_2 = {
+"type": "FeatureCollection",
+"name": "Lines_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 35.82979047874953, 34.107603859342824 ], [ 35.791551137693631, 34.007048555084722 ], [ 35.638121682839703, 34.02805658813395 ], [ 35.609324154390194, 33.966212715562058 ], [ 35.597993979262519, 33.922780377572636 ], [ 35.717904999363746, 33.846537740776 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 35.770779149959559, 33.903660707044693 ], [ 35.726874721339819, 33.839928471951524 ], [ 35.487052681137378, 33.893274713177661 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "3" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 35.651104175173494, 33.997134651848 ], [ 35.770779149959559, 33.903660707044693 ] ] ] } }
+]
+}
---
+++ data/Towers_1.js
@@ -0,0 +1,17 @@
+var json_Towers_1 = {
+"type": "FeatureCollection",
+"name": "Towers_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "3", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.609324154390194, 33.966212715562058 ] } },
+{ "type": "Feature", "properties": { "id": "4", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.597993979262519, 33.922780377572636 ] } },
+{ "type": "Feature", "properties": { "id": "5", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.487052681137378, 33.893274713177661 ] } },
+{ "type": "Feature", "properties": { "id": "6", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.717904999363746, 33.846537740776 ] } },
+{ "type": "Feature", "properties": { "id": "7", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.770779149959559, 33.903660707044693 ] } },
+{ "type": "Feature", "properties": { "id": "8", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.726874721339819, 33.839928471951524 ] } },
+{ "type": "Feature", "properties": { "id": "9", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.791551137693631, 34.007048555084722 ] } },
+{ "type": "Feature", "properties": { "id": "10", "status": "Up" }, "geometry": { "type": "Point", "coordinates": [ 35.82979047874953, 34.107603859342824 ] } },
+{ "type": "Feature", "properties": { "id": "1", "status": "Down" }, "geometry": { "type": "Point", "coordinates": [ 35.638121682839703, 34.02805658813395 ] } },
+{ "type": "Feature", "properties": { "id": "2", "status": "Down" }, "geometry": { "type": "Point", "coordinates": [ 35.651104175173494, 33.997134651848 ] } }
+]
+}
